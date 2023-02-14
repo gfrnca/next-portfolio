f8f84381034c6ae20ba75685ebb7fd6b5eade490
gfrnca <gabrielbfranca27@gmail.com>
2023-02-14 05:02:59 -0300

    Global: Project Setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+      <main>
+
+      </main>
+    </>
+  )
+}
