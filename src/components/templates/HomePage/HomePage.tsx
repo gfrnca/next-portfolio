f2310f7467096d5074732e42c5afbb8ccc435480
gfrnca <gabrielbfranca27@gmail.com>
2023-02-14 21:01:51 -0300

    Added: Section Wave

--- src/components/templates/HomePage/HomePage.tsx
+++ src/components/templates/HomePage/HomePage.tsx
@@ -7,11 +7,18 @@ import Hero from "../../organisms/HomePage/Hero/Hero";
 
 const HomePage = () => {
   return (
-    <Container>
-      <HomePageWrapper>
+    <HomePageWrapper>
+      <Container>
         <Hero />
-      </HomePageWrapper>
-    </Container>
+      </Container>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#09090a"
+          fill-opacity="1"
+          d="M0,128L48,117.3C96,107,192,85,288,112C384,139,480,213,576,218.7C672,224,768,160,864,144C960,128,1056,160,1152,165.3C1248,171,1344,149,1392,138.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+        ></path>
+      </svg>
+    </HomePageWrapper>
   );
 };
 
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,6 +3,7 @@
 const theme = {
   colors: {
     background: "#121214",
+    supportBackground: "#09090a",
     text: "#ededed",
     primary: "#8257e5",
     secondary: "#04d361",
