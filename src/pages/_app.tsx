5186bbdd5ea7fe3d2062e6df44e46421c9426189
gfrnca <gabrielbfranca27@gmail.com>
2023-02-23 10:56:34 -0300

    Added smooth opacity transitions when change route

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@
 import React from "react";
 import { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
+import { motion, AnimatePresence } from "framer-motion";
+import { useRouter } from "next/router";
 
 // CSS
 import GlobalStyle from "../styles/global";
@@ -12,10 +14,35 @@ import Navbar from "../components/organisms/Navbar/Navbar";
 import Footer from "../components/organisms/Footer/Footer";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const router = useRouter();
+
   return (
     <ThemeProvider theme={theme}>
       <Navbar />
-      <Component {...pageProps} />
+
+      <AnimatePresence exitBeforeEnter>
+        <motion.div
+          key={router.route}
+          initial="initialState"
+          animate="animateState"
+          transition={{
+            duration: 0.75,
+          }}
+          exit="exitState"
+          variants={{
+            initialState: {
+              opacity: 0,
+            },
+            animateState: {
+              opacity: 1,
+            },
+            exitState: {},
+          }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
+
       <Footer />
       <GlobalStyle />
     </ThemeProvider>
