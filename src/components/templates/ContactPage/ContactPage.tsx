6ce508d425dfe0d077faf3f28c841e6ddb9a6a71
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-07 10:58:16 -0300

    Created contact page file

---
+++ src/components/templates/ContactPage/ContactPage.tsx
@@ -0,0 +1,14 @@
+import { Container } from "@/src/styles/global"
+import { ContactPageWrapper } from "./ContactPage.styled"
+
+const ContactPage = () => {
+  return (
+    <ContactPageWrapper>
+        <Container className="container">
+            
+        </Container>
+    </ContactPageWrapper>
+  )
+}
+
+export default ContactPage
