92625a37f268dc45de9fcf3ee0cf278c17f74e21
gfrnca <gabrielbfranca27@gmail.com>
2023-03-08 10:50:29 -0300

    Stylized submit button

--- src/components/atoms/Buttons/Buttons.styled.ts
+++ src/components/atoms/Buttons/Buttons.styled.ts
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
 export const SubmitButton = styled.button `
+    padding: 12px;
+    border-radius: 4px;
+    border: none;
+    background: ${props => props.theme.colors.secondary.light}; 
+    cursor: pointer;
 
 `
--- src/components/organisms/ContactPage/ContactForm/ContactForm.tsx
+++ src/components/organisms/ContactPage/ContactForm/ContactForm.tsx
@@ -1,3 +1,4 @@
+import { SubmitButton } from "@/src/components/atoms/Buttons/Buttons.styled"
 import ContactInput from "@/src/components/atoms/ContactInput/ContactInput"
 import ContactTextArea from "@/src/components/atoms/ContactTextArea/ContactTextArea"
 import { ContactFormWrapper } from "./ContactForm.styled"
@@ -7,11 +8,15 @@ const ContactForm = () => {
     <ContactFormWrapper>
         {/* Name */}
         <ContactInput label="Name" type="text" placeholder="How you want to be called?" />
+
+        {/* E-mail */}
         <ContactInput label="E-mail" type="email" placeholder="Your best e-mail" />
         
+        {/* Message */}
         <ContactTextArea label="Message" />
 
-
+        {/* Submit Button */}
+        <SubmitButton>Submit</SubmitButton>
     </ContactFormWrapper>
   )
 }
