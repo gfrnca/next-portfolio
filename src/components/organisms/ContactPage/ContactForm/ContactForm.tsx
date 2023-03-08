fd08a242877a0a83e193428eabaf44ff682b763d
gfrnca <gabrielbfranca27@gmail.com>
2023-03-08 11:22:21 -0300

    Implemented e-mail validation

--- src/components/organisms/ContactPage/ContactForm/ContactForm.tsx
+++ src/components/organisms/ContactPage/ContactForm/ContactForm.tsx
@@ -1,11 +1,27 @@
 import { SubmitButton } from "@/src/components/atoms/Buttons/Buttons.styled"
 import ContactInput from "@/src/components/atoms/ContactInput/ContactInput"
 import ContactTextArea from "@/src/components/atoms/ContactTextArea/ContactTextArea"
-import { useState } from "react"
+import { use, useState } from "react"
 import { ContactFormWrapper } from "./ContactForm.styled"
 
 const ContactForm = () => {
+  // Name fill status
   const [nameStatus, setNameStatus] = useState(false);
+
+  // E-mail fill status
+  const [emailStatus, setEmailStatus] = useState(false);
+
+  // E-mail validation
+  const checkEmail = (e: any) => {
+    const filter =
+    /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+    if (!filter.test(e)) {
+      setEmailStatus(false)
+    } else {
+      setEmailStatus(true)
+    }
+  }
 
   return (
     <ContactFormWrapper>
@@ -13,7 +29,7 @@ const ContactForm = () => {
         <ContactInput className={nameStatus ? 'filled' : ''} onChange={(e) => {e.target.value.length > 3 && setNameStatus(true)}} label="Name" type="text" placeholder="How you want to be called?" />
 
         {/* E-mail */}
-        <ContactInput label="E-mail" type="email" placeholder="Your best e-mail" />
+        <ContactInput className={emailStatus ? 'filled' : ''} onChange={(e) => checkEmail(e.target.value)} label="E-mail" type="email" placeholder="Your best e-mail" />
         
         {/* Message */}
         <ContactTextArea placeholder="What you need to say?" label="Message" />
